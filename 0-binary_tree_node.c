03724f60e6b6007918da7f28a8d3b0855d2facb8
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-11-28 15:20:35 +0100

    fix betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
-/*
- * binary_tree_node - creates a binary tree node
+/**
+ * *binary_tree_node - creates a binary tree node
  *
  * @parent: parent node
  * @value: node value
@@ -14,12 +14,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
 
-	return (new)
+	return (new);
 }
